f8fe687213063d4f077395e1dbaa2a028fea395f
Deden Rustiana <rustianadeden25@gmail.com>
2022-06-24 20:52:32 +0700

    SpringBoot Swagger Integration for API Documentation

---
+++ src/main/java/com/api/springbootapi/swagger/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.api.springbootapi.swagger;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.api.springbootapi.controllers"))              
+          .paths(PathSelectors.any())                          
+          .build()
+          .apiInfo(apiInfo());                                           
+    }
+
+    private ApiInfo apiInfo(){
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot API", 
+                "Tutorial Spring Boot API", 
+                "1.0", 
+                "API TOS", 
+                new Contact("Deden Rustiana", "www.denrus.com", "denrus@gmail.com"), "www.denrus.com", "Apache", Collections.emptyList());
+        return apiInfo;
+    }
+}
